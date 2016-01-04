317c250ea9f5c525967d3257e702f24c7852458d
VISTALL <vistall.valeriy@gmail.com>
2016-01-04 18:06:56 +0300

    allow autoimport project on double click

--- ConsuloPlugin.cs
+++ ConsuloPlugin.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Net;
+using System.Reflection;
 using System.Text;
 using UnityEditor;
 using UnityEngine;
@@ -18,38 +19,38 @@ namespace MustBe.Consulo.Internal
 
 #if UNITY_BEFORE_5
 
-		[MenuItem("Edit/Use Consulo as External Editor", true)]
-		static bool UseConsuloAsExternalEditorValidator()
-		{
-			return !UseConsulo();
-		}
+	[MenuItem("Edit/Use Consulo as External Editor", true)]
+	static bool UseConsuloAsExternalEditorValidator()
+	{
+		return !UseConsulo();
+	}
 
-		[MenuItem("Edit/Disable Consulo as External Editor", true)]
-		static bool DisableConsuloAsExternalEditorValidator()
-		{
-			return UseConsulo();
-		}
+	[MenuItem("Edit/Disable Consulo as External Editor", true)]
+	static bool DisableConsuloAsExternalEditorValidator()
+	{
+		return UseConsulo();
+	}
 
-		[MenuItem("Edit/Use Consulo as External Editor")]
-		static void UseConsuloAsExternalEditor()
-		{
-			EditorPrefs.SetBool(ourEditorPrefsKey, true);
-		}
+	[MenuItem("Edit/Use Consulo as External Editor")]
+	static void UseConsuloAsExternalEditor()
+	{
+		EditorPrefs.SetBool(ourEditorPrefsKey, true);
+	}
 
-		[MenuItem("Edit/Disable Consulo as External Editor")]
-		static void DisableConsuloAsExternalEditor()
-		{
-			EditorPrefs.SetBool(ourEditorPrefsKey, false);
-		}
+	[MenuItem("Edit/Disable Consulo as External Editor")]
+	static void DisableConsuloAsExternalEditor()
+	{
+		EditorPrefs.SetBool(ourEditorPrefsKey, false);
+	}
 
 #else
 
-		[MenuItem("Edit/Use Consulo as External Editor", true)]
-		static bool ValidateUncheckConsulo()
-		{
-			Menu.SetChecked("Edit/Use Consulo as External Editor", UseConsulo());
-			return true;
-		}
+	[MenuItem("Edit/Use Consulo as External Editor", true)]
+	static bool ValidateUncheckConsulo()
+	{
+		Menu.SetChecked("Edit/Use Consulo as External Editor", UseConsulo());
+		return true;
+	}
 
 		[MenuItem("Edit/Use Consulo as External Editor")]
 		static void UncheckConsulo()
@@ -79,46 +80,54 @@ namespace MustBe.Consulo.Internal
 				return false;
 			}
 
-			var projectPath = ProjectPath();
-
-			var selected = EditorUtility.InstanceIDToObject(instanceID);
-
-			var filePath = projectPath + "/" + AssetDatabase.GetAssetPath(selected);
-
-			var request = WebRequest.Create("http://localhost:" + ourPort + "/api/unityOpenFile");
-			request.Timeout = 5000;
-			request.Method = "POST";
-
-			projectPath = projectPath.Replace('\\', '/');
-			filePath = filePath.Replace('\\', '/');
-			String json = "{" +
-			"projectPath: \"" + projectPath + "\"," +
-			"filePath: \"" + filePath + "\"," +
-			"contentType: \"" + selected.GetType().ToString() + "\"," +
-			"line: " + line +
-			"}";
-			request.ContentType = "application/json; charset=utf-8";
-			var bytes = Encoding.UTF8.GetBytes(json);
-			request.ContentLength = bytes.Length;
-
-			using (var stream = request.GetRequestStream())
+			try
 			{
-				stream.Write(bytes, 0, bytes.Length);
-			}
+				var projectPath = ProjectPath();
 
-			using (var requestGetResponse = request.GetResponse())
-			{
-				using (var responseStream = requestGetResponse.GetResponseStream())
+				var selected = EditorUtility.InstanceIDToObject(instanceID);
+
+				var filePath = projectPath + "/" + AssetDatabase.GetAssetPath(selected);
+
+				var request = WebRequest.Create("http://localhost:" + ourPort + "/api/unityOpenFile");
+				request.Timeout = 10000;
+				request.Method = "POST";
+
+				projectPath = projectPath.Replace('\\', '/');
+				filePath = filePath.Replace('\\', '/');
+				String json = "{" +
+				"projectPath: \"" + projectPath + "\"," +
+				"filePath: \"" + filePath + "\"," +
+				"editorPath: \"" + EditorApplication.applicationPath.Replace('\\', '/') + "\"," +
+				"contentType: \"" + selected.GetType().ToString() + "\"," +
+				"line: " + line +
+				"}";
+				request.ContentType = "application/json; charset=utf-8";
+				var bytes = Encoding.UTF8.GetBytes(json);
+				request.ContentLength = bytes.Length;
+
+				using (var stream = request.GetRequestStream())
 				{
-					using (var streamReader = new StreamReader(responseStream))
+					stream.Write(bytes, 0, bytes.Length);
+				}
+
+				using (var requestGetResponse = request.GetResponse())
+				{
+					using (var responseStream = requestGetResponse.GetResponseStream())
 					{
-						var result = streamReader.ReadToEnd();
-						if (result.Contains("unsupported-content-type"))
+						using (var streamReader = new StreamReader(responseStream))
 						{
-							return false;
+							var result = streamReader.ReadToEnd();
+							if (result.Contains("unsupported-content-type"))
+							{
+								return false;
+							}
 						}
 					}
 				}
+			}
+			catch(Exception e)
+			{
+				EditorUtility.DisplayDialog("Consulo Plugin", "Consulo is not accessible at http://localhost:" + ourPort + ", message: " + e.Message, "OK");
 			}
 			return true;
 		}
